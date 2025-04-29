e35be9afbd2678e65a3edee86aae4ba07848a3a3
vipul <rajvipulraj401@gmail.com>
2025-04-29 20:16:09 +0530

    For passing cypress error related to navbar usage instead of header component

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,14 +1,34 @@
+// import React from "react";
+// import styles from "./NavBar.module.css";
+
+// const NavBar = () => {
+//   return (
+//     <nav className={styles.navbar}>
+//       <div className={styles.title}>
+//         <h1>Bot AI</h1>
+//       </div>
+//       <div className={styles.toggle}>🌗 Theme</div> {/* Dummy Theme toggle */}
+//     </nav>
+//   );
+// };
+
+// export default NavBar;
+
+// ----v2 to pass cypress----
+
 import React from "react";
 import styles from "./NavBar.module.css";
 
 const NavBar = () => {
   return (
-    <nav className={styles.navbar}>
+    <header className={styles.navbar}>
+      {" "}
+      {/* changed from <nav> to <header> */}
       <div className={styles.title}>
         <h1>Bot AI</h1>
       </div>
-      <div className={styles.toggle}>🌗 Theme</div> {/* Dummy Theme toggle */}
-    </nav>
+      <div className={styles.toggle}>🌗 Theme</div>
+    </header>
   );
 };
 
